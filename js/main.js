4f9a854f0c3abf9b7f92715b1f9aae3cecf159db
Luan Nascimento <luann@id.uff.br>
2022-07-06 00:08:51 -0300

    Scroll through game list with keyboard arrows or up and down buttons

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let count = 1
+document.getElementById('radio1').checked = true;
+
+function nextGame(){
+    var game1 = "Game 1";
+    var game2 = "Game 2";
+    var game3 = "Game 3";
+    var game4 = "Game 4";
+    var game5 = "Game 5";
+
+    if(document.getElementById('radio1').checked){
+        document.getElementById('game1').innerHTML = '>>> ' + game1 + ' <<<';
+    }else{
+        document.getElementById('game1').innerHTML = game1;
+    }
+
+    if(document.getElementById('radio2').checked){
+        document.getElementById('game2').innerHTML = '>>> ' + game2 + ' <<<';
+    }else{
+        document.getElementById('game2').innerHTML = game2;
+    }
+
+    if(document.getElementById('radio3').checked){
+        document.getElementById('game3').innerHTML = '>>> ' + game3 + ' <<<';
+    }else{
+        document.getElementById('game3').innerHTML = game3;
+    }
+
+    if(document.getElementById('radio4').checked){
+        document.getElementById('game4').innerHTML = '>>> ' + game4 + ' <<<';
+    }else{
+        document.getElementById('game4').innerHTML = game4;
+    }
+
+    if(document.getElementById('radio5').checked){
+        document.getElementById('game5').innerHTML = '>>> ' + game5 + ' <<<';
+    }else{
+        document.getElementById('game5').innerHTML = game5;
+    }
+}
+
+function pressUp(){
+    if (count >= 2) {
+        count--;
+        document.getElementById('radio' + count).checked = true;
+        nextGame();
+    }
+}
+
+function pressDown(){
+    if (count <= 4) {
+        count++;
+        document.getElementById('radio' + count).checked = true;
+        nextGame();
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if ( ( e.code == "ArrowUp" ) && (count >= 2) ) {
+        count--;
+        document.getElementById('radio' + count).checked = true;
+        nextGame();
+    } else if ( (e.code == "ArrowDown")  && (count <= 4)) {
+        count++;
+        document.getElementById('radio' + count).checked = true;
+        nextGame();
+    }
+});
+
+nextGame();

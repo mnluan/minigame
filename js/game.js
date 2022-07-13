3a8ff66e4f6e117bf560a7627bd380d1aebc015a
Luan Nascimento <luann@id.uff.br>
2022-07-13 20:32:18 -0300

    move car rival

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 let params = new URLSearchParams(location.search);
+let isOver = false;
 var idgame = params.get('id');
 
 function gameConfig(){
     if (idgame != "" && idgame == "game_1"){
-        console.log(true);
         document.getElementById('screen').innerHTML = '' + 
             '<div class="over"></div>' +
             '<div class="line"></div>' +
@@ -15,7 +15,47 @@ function gameConfig(){
             '<div class="player"></div>'+
             '<div class="rival"></div>';
     }
-    console.log(false);
+}
+
+function moverivalcar(){
+    var moveEnemies = setInterval(()=>{
+        var enemies = document.getElementsByClassName("rival");
+    
+        if(enemies != undefined){
+            for (var i = 0; i < enemies.length; i++){
+                var enemy = enemies[i]; //getting each enemies
+                var enemyLeft = parseInt(
+                    window.getComputedStyle(enemy).getPropertyValue("left")
+                );
+                if (enemyLeft < -210){
+                    enemy.parentElement.removeChild(enemy); 
+                    //score++;
+                }
+                enemy.style.left = enemyLeft + -20 + "px"
+            }
+        }
+    }, 200);
+}
+
+function movecarUp(){
+    
+}
+
+function movecarDown(){
+    
+}
+
+function pressUp(){
+    if (idgame != "" && idgame == "game_1"){
+        movecarUp();
+    }
+}
+
+function pressDown(){
+    if (idgame != "" && idgame == "game_1"){
+        movecarDown();
+    }
 }
 
 gameConfig();
+moverivalcar();
